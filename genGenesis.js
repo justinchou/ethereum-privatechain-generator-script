c167ebf4e99147c28d04a1629b3e6a31ea2dc962
Justin <zhou78620051@126.com>
2018-05-01 21:29:53 +0800

    Rename the script and do some lint

--- genGenesis.js
+++ genGenesis.js
@@ -1,4 +1,4 @@
-var genesis = {
+const genesis = {
   "config": {
     "chainId": 1224,
     "homesteadBlock": 0,
@@ -15,28 +15,28 @@ var genesis = {
   "mixhash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
   "parentHash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
   "timestamp" : "0x00"
-}
+};
 
-var cash = {"balance": "0x821ab0d4414980000"}
-var prefix = '0x'
+const cash = {"balance": "0x821ab0d4414980000"};
+const prefix = '0x';
 
-var args = process.argv.length
-if (args <= 2) process.exit()
+const args = process.argv.length;
+if (args <= 2) process.exit();
 
-var example = '0cd59780cc3da5f1d6866ede6dcc6dafbc24e3a2'
-var accounts = process.argv.splice(2, args)
+const example = '0cd59780cc3da5f1d6866ede6dcc6dafbc24e3a2';
+const accounts = process.argv.splice(2, args);
 
 accounts.forEach((account) => {
   if (account.length !== example.length) {
-    console.log('invalid account')
+    console.log('invalid account');
     process.exit()
   }
-})
+});
 
 //console.log(accounts)
 accounts.forEach((account) => {
   genesis.alloc[prefix + account] = cash
-})
+});
 
-var genesisBlock = JSON.stringify(genesis, null, 2)
-require('fs').writeFileSync('./genesis.json', genesisBlock)
+const genesisBlock = JSON.stringify(genesis, null, 2);
+require('fs').writeFileSync('./genesis.json', genesisBlock);
